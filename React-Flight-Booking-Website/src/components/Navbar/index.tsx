3b2d6dc932e5068297b7e9a6a51b4a3280870553
Md Ahad Hossain <117075956+Ahadhossan@users.noreply.github.com>
2025-06-25 12:33:07 +0600

    Update index.tsx

--- React-Flight-Booking-Website/src/components/Navbar/index.tsx
+++ React-Flight-Booking-Website/src/components/Navbar/index.tsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
 	const navLinks = [
 		{ title: "Home", path: "/", active: true },
-		{ title: "How To", path: "/howTo", active: false },
-		{ title: "Services", path: "/services", active: false },
-		{ title: "Blog", path: "/blog", active: false },
-		{ title: "Contact", path: "/contact", active: false },
-		{ title: "Newspaper", path: "/newspaper", active: false },
-		{ title: "About", path: "/about", active: false },
+		{ title: "How To", path: "/", active: false },
+		{ title: "Services", path: "/", active: false },
+		{ title: "Blog", path: "/", active: false },
+		{ title: "Contact", path: "/", active: false },
+		{ title: "Newspaper", path: "/", active: false },
+		{ title: "About", path: "/", active: false },
 	];
 	return (
 		<div className="z-[2] w-full my-auto mx-0 h-16 md:px-[60px] px-8 py-10 flex items-center justify-between absolute top-[20px]">
@@ -18,7 +18,7 @@ const Navbar = () => {
 				{navLinks.map((navLink) => {
 					return (
 						<Link
-							to={navLink.path}
+							to="/"
 							key={navLink.title}
 						>
 							<span
